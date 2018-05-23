6037bd516508c4b71bf052f889bc01b6c566b0a9
深情小建 <374452668@qq.com>
2018-05-23 22:23:54 +0800

    在随机位置生成星星

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -31,10 +31,33 @@ cc.Class({
             type: cc.Node
         }
     },
+    
+    spawnNewStar () {
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.starPrefab);
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+        newStar.setPosition(this.getNewStarPosition());
+    },
 
-    // LIFE-CYCLE CALLBACKS:
+    getNewStarPosition () {
+        var randX = 0;
+        // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
+        var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
+        // 根据屏幕宽度，随机得到一个星星 x 坐标
+        var maxX = this.node.width/2;
+        randX = cc.randomMinus1To1() * maxX;
+        // 返回星星坐标
+        return cc.p(randX, randY);
+    },
 
-    // onLoad () {},
+	onLoad () {
+		// 获取地平面的 y 轴坐标
+        this.groundY = this.ground.y + this.ground.height/2;
+        // 生成一个新的星星
+        this.spawnNewStar();
+	},
 
     start () {
 
