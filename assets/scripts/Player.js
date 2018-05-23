a47118b93062424a7fb1df8a77a0e9ca874bb548
深情小建 <374452668@qq.com>
2018-05-23 21:19:28 +0800

    1、编写组件属性 2、编写跳跃和移动代码

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -12,26 +12,30 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        // 主角跳跃高度
+        jumpHeight: 0,
+        // 主角跳跃持续时间
+        jumpDuration: 0,
+        // 最大移动速度
+        maxMoveSpeed: 0,
+        // 加速度
+        accel: 0
     },
+    
+	setJumpAction: function () {
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+   	},
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
+   	onLoad () {
+	   	// 初始化跳跃动作
+	    this.jumpAction = this.setJumpAction();
+	    this.node.runAction(this.jumpAction);
+   	},
 
     start () {
 
